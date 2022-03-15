afb65d483faf1c817a9c69c3ab506b6533bf024e
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2022-03-15 15:49:27 +1100

    login first version

--- newsback/src/views/login/Login.js
+++ newsback/src/views/login/Login.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import {
+  React, 
+  Component,
+} from 'react';
+import './Login.css'
+import { GooglePlusOutlined,FacebookOutlined,TwitterOutlined } from '@ant-design/icons';
 
-export default function login() {
-  return (
-    <div>login</div>
-  )
+class Login extends Component {
+
+  render(){
+    return(
+      <div className='lg_bg'>
+        <p className='lg_tit'><br/>New System</p>
+        <div className='lg_cb'>
+          <p className='lg_bg_tit'>Login</p>
+          <p className='lg_bg_sbtit'>Please Enter Your Username and Password</p>
+          <input className='lg_bg_email' placeholder='Username'></input>
+          <input className='lg_bg_pw' placeholder='Password' type="password"></input> 
+          <p className='lg_bg_fp'>Forget Password</p>
+          <p className='lg_bg_su'>Sign up</p>
+          <button className='lg_bg_btn_lg'>Login</button>
+          <br/><br/>
+          <GooglePlusOutlined className='lg_bg_g'/>
+          <FacebookOutlined className='lg_bg_fb'/>
+          <TwitterOutlined className='lg_bg_tt'/>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Login;
+  
